83002a697dc38f9cab9587c459a537257e4dae2c
FahadAkash <fahad12345ff8@gmail.com>
2025-01-07 11:00:52 +0600

    addded

--- event/Scripts/activate.bat
+++ event/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=F:\gihtub\Event-Management-System\Python\event"
+set "VIRTUAL_ENV=F:\gihtub\event_management_system\event"
 
 if not defined PROMPT set PROMPT=$P$G
 
